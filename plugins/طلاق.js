4d2164031410e70bfe47408d091995516dacaade
Gghjjkkk <119447588+Gghjjkkk@users.noreply.github.com>
2025-10-28 14:47:19 +0300

    Update طلاق.js

--- plugins/طلاق.js
+++ plugins/طلاق.js
@@ -1,18 +1,29 @@
-let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`${toM(a)}, 
-${toM(b)},* \n\*لم تصبح زوجين بعد الان 💔*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
-handler.tags = ['main', 'fun']
-handler.command = ['الطلاق','طلاق']
+let handler = async (m, { groupMetadata, conn }) => {
+    let participants = groupMetadata.participants.map(v => v.id)
+    
+    let a = participants[Math.floor(Math.random() * participants.length)]
+    let b
+    do {
+        b = participants[Math.floor(Math.random() * participants.length)]
+    } while (b === a)
+    
+    let text = `💔 *إعلان طلاق* 💔\n\n`
+    text += `  @${a.split('@')[0]}\n`
+    text += `  @${b.split('@')[0]}\n\n`
+    text += `لم تصبحا زوجين بعد الآن 💔\n`
+   
+    
+    await conn.sendMessage(m.chat, { 
+        text: text,
+        mentions: [a, b]
+    }, { quoted: m })
+}
+
+handler.help = ['طلاق']
+handler.tags = ['fun', 'group']
+handler.command = ['الطلاق', 'طلاق']
 handler.group = true
 handler.limit = 4
-handler.prem
+handler.premium = false
+
 export default handler
