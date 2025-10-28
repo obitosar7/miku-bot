dc78087641d73a025f6bfc04725de6935143361e
Gghjjkkk <119447588+Gghjjkkk@users.noreply.github.com>
2025-10-28 19:19:35 +0300

    Update صداقه.js

--- plugins/صداقه.js
+++ plugins/صداقه.js
@@ -1,15 +1,27 @@
-let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*لنكوّن بعض الأصدقاء*\n\n*يا ${toM(a)} لتتحدث ف الخاص ${toM(b)} حتى يتمكنوا من اللعب ويصبحوا أصدقاء 🙆*\n\n*تبدأ أفضل الصداقات بالالعاب 😉*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['amistad']
-handler.tags = ['main', 'fun']
-handler.command = ['صداقه','صداقة']
+let handler = async (m, { groupMetadata, conn }) => {
+    let participants = groupMetadata.participants.map(v => v.id)
+    
+    let a = participants[Math.floor(Math.random() * participants.length)]
+    let b
+    do {
+        b = participants[Math.floor(Math.random() * participants.length)]
+    } while (b === a)
+    
+    let text = `*لنكوّن بعض الأصدقاء*\n\n`
+    text += `*يا @${a.split('@')[0]} لتتحدث ف الخاص مع @${b.split('@')[0]} حتى يتمكنوا من اللعب ويصبحوا أصدقاء 🙆*\n\n`
+    text += `*تبدأ أفضل الصداقات بالالعاب 😉*`
+    
+    await conn.sendMessage(m.chat, { 
+        text: text,
+        mentions: [a, b]
+    }, { quoted: m })
+}
+
+handler.help = ['صداقة']
+handler.tags = ['fun', 'group']
+handler.command = ['صداقه', 'صداقة']
 handler.group = true
+handler.limit = 4
+handler.premium = false
+
 export default handler
